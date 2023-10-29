cfbe39fe95d2687e1450b1d282410c348205719c
LightAstral <140429130+LightAstral@users.noreply.github.com>
2023-10-29 17:50:00 +0200

    Рабочая регистрация и вход пользовтелей с БД и ЛК с ифнормацией о пользователе и изменениями данных пользователя.

--- solar_hosting/models.py
+++ solar_hosting/models.py
@@ -1,21 +1,3 @@
-# from django.contrib.auth.models import AbstractUser
-#
-#
-# # Create your models here.
-#
-#
-# class User(AbstractUser):
-#     email = models.EmailField(unique=True)
-#     phone = models.CharField(max_length=15)
-#     password1 = models.CharField(max_length=128, default='')
-#     password2 = models.CharField(max_length=128, default='')
-#
-#     # Установите related_name для полей groups и user_permissions
-#     groups = models.ManyToManyField('auth.Group', related_name='custom_users')
-#     user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_users_permissions')
-#
-#     def __str__(self):
-#         return self.username
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Permission, Group
 
@@ -30,7 +12,3 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
-    # Измените related_name для groups и user_permissions
-    # groups = models.ManyToManyField(Group, verbose_name='Groups', blank=True, related_name='solar_hosting_users')
-    # user_permissions = models.ManyToManyField(Permission, verbose_name='User permissions', blank=True,
-    #                                           related_name='solar_hosting_users_permissions')
